2a5aea2f4f70b73e5bd53e15279bbc2fe03d16f6
benNurtjipta <ben@nurtjipta.de>
2025-03-26 14:08:34 +0100

    card render

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,35 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import GameStart from "./components/GameStart";
+import GameFinish from "./components/GameFinish";
 import Welcome from "./components/Welcome";
 import Footer from "./components/Footer";
 
 const App = () => {
   const [isStarted, setIsStarted] = useState(false);
-  const [isFinished, setisFinished] = useState(false);
+  const [isFinished, setIsFinished] = useState(false);
   const [cardsComplete, setCardsComplete] = useState([]);
   console.log(cardsComplete);
+  const [cardBack, setCardBack] = useState(); //option later
 
   return (
     <>
       <Header />
       {isStarted ? (
-        <GameStart cardsComplete={cardsComplete} />
+        <GameStart
+          cardsComplete={cardsComplete}
+          cardBack={cardBack}
+          setIsFinished={setIsFinished}
+          setCardsComplete={setCardsComplete}
+        />
       ) : (
         <Welcome
           setIsStarted={setIsStarted}
           setCardsComplete={setCardsComplete}
+          setCardBack={setCardBack}
         />
       )}
+      {isFinished && <GameFinish />}
       <Footer />
     </>
   );
--- src/components/GameStart.jsx
+++ src/components/GameStart.jsx
@@ -1,5 +1,24 @@
-const GameStart = ({ cardsComplete }) => {
-  return <div></div>;
+import CardCreater from "./CardCreater/CardCreater";
+
+const GameStart = ({
+  cardsComplete,
+  cardBack,
+  setIsFinished,
+  setCardsComplete,
+}) => {
+  return (
+    <div className="flex justify-center items-center">
+      {cardsComplete.map((card) => (
+        <CardCreater
+          key={card.id}
+          card={card}
+          back={cardBack}
+          setCardsComplete={setCardsComplete}
+          cardsComplete={cardsComplete}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default GameStart;
