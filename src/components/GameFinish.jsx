0e29aee94999405fa8fe2bcc9c537b2d1a953251
SarahDomscheit <sarri91@googlemail.com>
2025-04-02 15:25:39 +0200

    counter + finishstyling

--- src/components/GameFinish.jsx
+++ src/components/GameFinish.jsx
@@ -1,18 +1,22 @@
 import ConfettiAnimation from "./ConfettiAnimation";
-import "./GameFinish.css"
-const GameFinish = ({result, setIsStarted, setIsFinished}) => {
-
-  function Restart(){
-    setIsStarted(false)
-    setIsFinished(false)
+import "./GameFinish.css";
+const GameFinish = ({ result, setIsStarted, setIsFinished }) => {
+  function Restart() {
+    setIsStarted(false);
+    setIsFinished(false);
   }
 
-  return <div className="finishedBox"><div>{result}</div>;
-  {result==="Gewonnen! 🎉" && <ConfettiAnimation/>}
-  <button className="button" onClick={Restart}>
-  Zur Übersicht
-</button>
-  </div>
+  return (
+    <div className="finishedBox">
+      <div className="finishCard">
+        <div className="text-white text-2xl">{result}</div>
+        {result === "Gewonnen! 🎉" && <ConfettiAnimation />}
+        <button className="button mt-4 " onClick={Restart}>
+          Zur Übersicht
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default GameFinish;
